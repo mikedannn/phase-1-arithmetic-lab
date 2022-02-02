74700a1e790d4e6cd2a6d3309eff022c7e879ab4
Michael <michael.daniels1013@gmail.com>
2022-02-01 21:49:46 -0600

    commit tree 1482ab2974181ef55e1818a81f4325806d0f889b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = (num1*num2);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 14;
+const num4 = 5;
+const mod = (num3 % num4);
+
+const max = Math.max(2, 4, 5, 8, 20);
